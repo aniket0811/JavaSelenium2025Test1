04816dfbf58c3b268e30ce87163426eb289c0415
aniket0811 <83530588+aniket0811@users.noreply.github.com>
2025-07-20 12:20:20 +0530

    Branch A Changes

--- src/p2/Class2.java
+++ src/p2/Class2.java
@@ -1,8 +1,9 @@
 package p2;
 
 public class Class2 {
-	
+
 	public static void main(String[] args) {
-		System.out.println("P2 c2 world");
+		System.out.println("P2 c2 world,,,,,");
+		System.out.println("In Branch A");
 	}
 }
